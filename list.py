d097ef6cfa03b45dc02c7a0384dd257df1910066
Dhanenthira  M <dhanenthira2005@gmail.com>
2026-08-20 18:53:39 +0530

    add list.py with initial content for basic data structures and operations

---
+++ list.py
@@ -0,0 +1,27 @@
+a=[1,2,3,4,5]
+print(a)
+#List Length
+b=["dhane","karu","kirthi"]
+print(len(b))
+
+
+
+print(bool("111111115"))
+
+print(10>9)
+
+print({"name":"dhane"})
+age = 36
+#This will produce an error:
+for i in range(2):
+    print(i)
+    
+text = "Hello"
+data = text.encode()
+
+print(data)
+
+x = 3+5j
+y = 5j
+z = -5j
+print(x)
