a4c48d079e8dbc1642fb3b5549221fb55cbf5c9f
Dhanenthira  M <dhanenthira2005@gmail.com>
2026-08-24 17:58:22 +0530

    Add function examples for greeting, staff calculation, user authentication, and animal descriptions; implement decorators for enhanced functionality.

---
+++ function.py
@@ -0,0 +1,52 @@
+def dhane():
+    print("hello")
+    
+dhane()
+#return functions
+def staff():
+     return 10+30
+print(staff())
+# def chick():
+#     s_name="dhaen"
+#     s_pass="123"
+#     a=int(input("dhaen"))
+#     b=int(input("123"))
+#     if a==s_name and b==s_pass:
+#         print("pass correct")
+#     else:
+#         print("pass incorrect")
+        
+# chick()
+def chick():
+    
+    s_name = "dhaen"
+    s_pass = "123"
+
+    a = input("Enter name: ")
+    b = input("Enter password: ")
+
+    if a == s_name and b == s_pass:
+        print("pass correct")
+    else:
+        print("pass incorrect")
+
+chick()
+
+def animal(a,b):
+    print("the "+a+" is very good boy")
+    print("the "+b+" is very cute ")
+animal(a="dog",b="cat")
+
+#decorators
+        
+def name(fun):
+    def dhane():
+        print("hello")
+        fun()
+    return dhane
+
+@name
+def move():
+    print("hello world")
+
+move()
