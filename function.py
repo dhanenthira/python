6e00f74c31fdb7c834a8a6f8102b3d457dda3abc
Dhanenthira  M <dhanenthira2005@gmail.com>
2026-08-25 19:49:30 +0530

    Refactor function.py to uncomment and organize function definitions for improved readability and functionality

--- function.py
+++ function.py
@@ -1,82 +1,96 @@
-def dhane():
-    print("hello")
+# def dhane():
+#     print("hello")
     
-dhane()
-#return functions
-def staff():
-     return 10+30
-print(staff())
+# dhane()
+# #return functions
+# def staff():
+#      return 10+30
+# print(staff())
+# # def chick():
+# #     s_name="dhaen"
+# #     s_pass="123"
+# #     a=int(input("dhaen"))
+# #     b=int(input("123"))
+# #     if a==s_name and b==s_pass:
+# #         print("pass correct")
+# #     else:
+# #         print("pass incorrect")
+        
+# # chick()
 # def chick():
-#     s_name="dhaen"
-#     s_pass="123"
-#     a=int(input("dhaen"))
-#     b=int(input("123"))
-#     if a==s_name and b==s_pass:
+    
+#     s_name = "dhaen"
+#     s_pass = "123"
+
+#     a = input("Enter name: ")
+#     b = input("Enter password: ")
+
+#     if a == s_name and b == s_pass:
 #         print("pass correct")
 #     else:
 #         print("pass incorrect")
-        
+
 # chick()
-def chick():
-    
-    s_name = "dhaen"
-    s_pass = "123"
 
-    a = input("Enter name: ")
-    b = input("Enter password: ")
+# def animal(a,b):
+#     print("the "+a+" is very good boy")
+#     print("the "+b+" is very cute ")
+# animal(a="dog",b="cat")
 
-    if a == s_name and b == s_pass:
-        print("pass correct")
-    else:
-        print("pass incorrect")
-
-chick()
-
-def animal(a,b):
-    print("the "+a+" is very good boy")
-    print("the "+b+" is very cute ")
-animal(a="dog",b="cat")
-
-#decorators
+# #decorators
         
-def name(fun):
-    def dhane():
-        print("hello")
-        fun()
-    return dhane
+# def name(fun):
+#     def dhane():
+#         print("hello")
+#         fun()
+#     return dhane
 
-@name
-def move():
-    print("hello world")
+# @name
+# def move():
+#     print("hello world")
 
-move()  
+# move()  
 
 
-def my_name(fun):
-    def cap():
-        return fun().upper()
-    return cap
-@my_name
-def name():
-    return "hello"
+# def my_name(fun):
+#     def cap():
+#         return fun().upper()
+#     return cap
+# @my_name
+# def name():
+#     return "hello"
     
-@my_name
-def clas():
-    return "cse"
+# @my_name
+# def clas():
+#     return "cse"
     
-print(clas())
-print(name())
+# print(clas())
+# print(name())
 
-def name(fun):
-    def call():
-        print("my name is :")
-        fun()
-        print("this is my name ")
-    return call
+# def name(fun):
+#     def call():
+#         print("my name is :")
+#         fun()
+#         print("this is my name ")
+#     return call
     
-@name
-def my():
-    print("dhane")
+# @name
+# def my():
+#     print("dhane")
     
-my()
+# my()
     
+    
+def dhane(dhane):
+    return dhane+10
+
+print(dhane(10))
+
+
+def len():
+    return "welcome to my channel"
+
+dh=len()
+print(dh)
+
+
