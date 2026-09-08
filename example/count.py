a4ee093c313525bdd137e936c01fe27af1af701d
Dhanenthira  M <dhanenthira2005@gmail.com>
2026-09-08 13:48:50 +0530

    Add functions for sorting input and swapping case

---
+++ example/count.py
@@ -0,0 +1,39 @@
+# def arr():
+#     a=[]
+#     word=""
+#     n=int(input("enter the number of words"))
+#     for i in range(n):
+#         word=input("enter the words:")
+#         a.append(word)
+#     print(a)
+    
+# arr()
+
+# def f():
+#     for i in range(1,5):
+#         for j in  range(2):
+#             print(j)
+#             print(end=" ")
+# f()
+
+
+# def star():
+#     n=int(input())
+#     for i in range(1,n+1):
+#         for j in range(1,i+1):
+#             print(j, end="")
+#         print()
+        
+# star()
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()
+
+
+def apl():
+    a = input()
+    a =(sorted(a))
+    print(a)
+
+apl()
---
+++ example/swap.py
@@ -0,0 +1,5 @@
+def swapcase():
+    a=input()
+    print(a.swapcase())
+    
+swapcase()
